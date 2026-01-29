2f161a29c1e88686fa4d618ced81674e951604d9
leeglb <galbraithlee76@gmail.com>
2026-01-29 21:28:45 +1000

    yes new done

---
+++ 69 Sqrt(X).py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        square_value = int(x ** 0.5) // 1
+
+        return square_value
---
+++ README.md
@@ -0,0 +1,5 @@
+<h1> LeetCode Folder And Progress </h1>
+
+In this folder, I will contain my leetcode questions that I have completed.
+And also all solutions will be visible to anyone who requires help.
+
